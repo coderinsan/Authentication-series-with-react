97a017f2aaefc855cac309662a246b054444d263
Krishna Bansal <140575587+coderinsan@users.noreply.github.com>
2023-08-15 17:11:44 +0530

    "

--- server/utils/GoogleStrategy.js
+++ server/utils/GoogleStrategy.js
@@ -1,8 +1,8 @@
 const GoogleStratgy = require("passport-google-oauth20").Strategy;
 
 exports.GoogleStratgyPassport = new  GoogleStratgy({
-    clientID:"180074994433-j7jddd55m3v63qo7uh9hptf86ms78evg.apps.googleusercontent.com",
-    clientSecret:"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",
+    clientID:"xxx.com",
+    clientSecret:"xxx4",
     callbackURL:"http://localhost:4000/api/v1/google/callback",
 
 },(accesstoken,refreshtoken,profile,done)=>{
